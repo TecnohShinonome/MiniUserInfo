3d5abc4a6394cefebf54e8c457625aa6f4a84bc0
tecnoh <216894163+TecnohShinonome@users.noreply.github.com>
2025-07-07 02:54:28 +0900

    Update client.lua comments for player name handling

--- client/client.lua
+++ client/client.lua
@@ -1,18 +1,23 @@
 -- If you are not using QBCore, please comment out the following line.
--- local QBCore = exports['qb-core']:GetCoreObject()
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('MiniUserInfo')
 AddEventHandler('MiniUserInfo', function(date,time,weekday)
+    -- If you are not using QBCore, please comment out the following line.
+    -- You will need to handle player data acquisition differently if QBCore is not used.
+    local PlayerData = QBCore.Functions.GetPlayerData() -- Get player data
+
     SendNUIMessage({
         action = "setMiniUserInfo",
         date = date,
         weekday = Config.WeekDays[weekday+1],
         time = time,
-        name = GetPlayerName(NetworkGetEntityOwner(PlayerPedId())),
+        -- If you are not using QBCore, use the line below this comment block.
+        -- name = GetPlayerName(NetworkGetEntityOwner(PlayerPedId())),
+        name = PlayerData.charinfo.firstname .. " " .. PlayerData.charinfo.lastname,
         userid = GetPlayerServerId(NetworkGetEntityOwner(PlayerPedId())),
-        -- If you are not using QBCore, please comment out.
-        jobname = QBCore.Functions.GetPlayerData().job.name .. " " .. QBCore.Functions.GetPlayerData().job.grade.name,
+        -- If you are not using QBCore, please comment out the following line.
+        jobname = PlayerData.job.name .. " " .. PlayerData.job.grade.name,
         server = Config.ServerName
     })
 end)
